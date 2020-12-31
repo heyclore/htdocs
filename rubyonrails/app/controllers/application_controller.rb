a9a1e897f44127b575df4bc7811f6d5d0e9932e1
heyclore <cloore@gmail.com>
2020-12-31 17:38:05 +0700

    customize an error on register page

--- rubyonrails/app/controllers/application_controller.rb
+++ rubyonrails/app/controllers/application_controller.rb
@@ -8,3 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
+
+ActionView::Base.field_error_proc = proc do |html_tag, instance|
+  html_tag.gsub("form-control", "form-control is-invalid").html_safe
+end
