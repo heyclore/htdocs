56dd95ac37a543807828b28200538ca8c577008f
heyclore <cloore@gmail.com>
2021-01-03 03:38:36 +0700

    add pagination

--- rubyonrails/app/controllers/timelines_controller.rb
+++ rubyonrails/app/controllers/timelines_controller.rb
@@ -5,7 +5,7 @@ class TimelinesController < ApplicationController
   # GET /timelines
   # GET /timelines.json
   def index
-    @timelines = Timeline.all.paginate(page: params[:page], per_page: 1)
+    @timelines = Timeline.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
     @timeline = Timeline.new
   end
 
